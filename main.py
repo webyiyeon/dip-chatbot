f24d4c132fa05f2ed15c4d4b2853a99dc47bfa10
webyiyeon <97453781+webyiyeon@users.noreply.github.com>
2024-08-09 15:43:41 +0900

    add tavily api key input

--- main.py
+++ main.py
@@ -12,17 +12,31 @@ st.markdown("""
 st.divider()
 st.subheader("API 키 설정")
 st.markdown("""
-            > OpenAI API 키 발급 방법은 아래 링크를 참고해주세요!
+            > API 키 발급은 아래 링크를 참고해주세요.
             >
-            > ℹ️ [발급 방법](https://wikidocs.net/233342) (<랭체인LangChain 노트> - LangChain 한국어 튜토리얼🇰🇷)
+            > ℹ️ [OpenAI API 발급 방법](https://wikidocs.net/233342) (LangChain 한국어 튜토리얼🇰🇷)
+            >
+            > ℹ️ [Tavily Search API 발급](https://app.tavily.com/sign-in) (Tavily website)
             """)
 
 # API 키 입력
-api_key = st.text_input("API 키 입력", type="password")
+api_key = st.text_input("OpenAI API 키 입력(ChatGPT)", type="password")
+
+# TAVILY_API_KEY
+search_api_key = st.text_input("Tavily Search API 키 입력(검색용, agent 페이지 필수값입니다.)", type="password")
 
 # 설정 확인 버튼
 confirm_btn = st.button("설정하기", key=api_key)
 
 if confirm_btn:
-    os.environ["OPENAI_API_KEY"] = api_key
-    st.write(f"API 키가 설정되었습니다.: `{api_key[:15]}`********")
+    if api_key:
+        os.environ["OPENAI_API_KEY"] = api_key
+        st.write(f"OpenAI API 키가 설정되었습니다.: `{api_key[:15]}`********")
+    
+    if search_api_key:
+        os.environ["TAVILY_API_KEY"] = search_api_key
+        st.write(f"Tavily Search API 키가 설정되었습니다.: `{search_api_key[:15]}`********")
+
+    
+
+
